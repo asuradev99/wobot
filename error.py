43df244068a339d24ea576030fe33d95db9066cf
Aditya Suresh <ethanaditya@gmail.com>
2021-09-18 11:17:05 -0600

    minor imporvements

--- error.py
+++ error.py
@@ -7,7 +7,12 @@ class ErrorHandler(commands.Cog):
     
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        print(error)
+        print(str(type(error)) + str(error))
         if isinstance(error, commands.CommandNotFound):
             embed=discord.Embed(title="**Command Not Found**", description="The command you typed was not found. See `help` for a list of all the commands available.", color=0xe01b24)
             await ctx.send(embed=embed)
+        if isinstance(error, (commands.errors.CommandInvokeError)):
+            embed=discord.Embed(title="**Command Error**", description=f"{str(error.original)}", color=0xe01b24)
+            await ctx.send(embed=embed)
+            print(error.original)
+        
